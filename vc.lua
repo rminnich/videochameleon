95bc47ac1830680f981d0b3849b7c617ea33a05e
Charlie Mooney <charliemooney@chromium.org>
2012-10-12 10:42:52 -0700

    Add function to save .ppm images from frame buf
    This adds the saveimage() function to the videochameleon console.  It
    takes a chunk of the frame buffer and reads it out, parses it and saves
    it to disk in the plain text image file format .ppm.
    
    Usage:  saveimage(filename, x, y, w, h)
       ie:  saveimage("img.ppm", 0, 0, 150, 150)
    
    It will handle multiple pixel requests for you, but only if the width of
    the requested section can fit in a single requests.  It won't work if
    w > 170!
    
    Signed-off-by: Charlie Mooney <charliemooney@chromium.org>

--- vc.lua
+++ vc.lua
@@ -69,15 +69,23 @@ function getcurrentedid()
 	return edid
 end
 
+local clock = os.clock
+function sleep(n)  -- seconds
+  local t0 = clock()
+  while clock() - t0 <= n do end
+end
+
 function dumppixels(x, y, w, h)
 	maxpixels = HOST_BUFFER_SIZE / PIXEL_STORAGE_SIZE
 	if (w * h > maxpixels) then
 		error("Too many pixels requested!")
 	else
+		print("grabpixels")
 		vc("grabpixels", 3, x, y, w, h)
+		print("readmem")
 		r, mem = readmem(HOST_BUFFER_LOCATION,
 				PIXEL_STORAGE_SIZE * w * h)
-		return mem
+		return parsepixels(mem, x, y, w, h)
 	end
 end
 
@@ -85,25 +93,50 @@ function parsepixels(mem, x, y, w, h)
 	i = 1
 	cx = x
 	cy = y
-	print("Pixel\tRed\tGreen\tBlue")
+	pixelnum = 1
+	pixels = {}
 	while (i < string.len(mem)) do
-		io.write(string.format("(%d, %d)\t", cx, cy))
-		for channel = 0,2 do
-			value = string.byte(mem, i)
-			value = value + (256 * string.byte(mem, i+1))
-			io.write(string.format("%d\t", value))
+		color = {}
+		for chan = 1,3 do
+			color[chan] = string.byte(mem, i)
+			color[chan] = color[chan] + (256 * string.byte(mem, i+1))
 			i = i + 2
 		end
-		io.write(string.format("\n"))
+		pixels[pixelnum] = color
+		pixelnum = pixelnum + 1
 		cx = cx + 1
 		if (cx - x >= w) then
 			cx = x
 			cy = cy + 1
 		end
 	end
+	return pixels
 end
 
-function readhostbuffer()
-	r, mem = readmem(HOST_BUFFER_LOCATION, HOST_BUFFER_SIZE)
-	return mem
+function saveimage(filename, x, y, w, h)
+	maxpixels = HOST_BUFFER_SIZE / PIXEL_STORAGE_SIZE
+	rowsperreq = math.min(math.floor(maxpixels / w), h)
+	if (w > maxpixels) then
+		error("Requested area is too wide!")
+		return
+	end
+
+	of = io.open(filename,"w")
+	of:write(string.format("P3\n%d %d\n1024\n", w, h))
+
+	for req = 1,math.ceil(h / rowsperreq) do
+		pixels = dumppixels(x, y + (req - 1) * rowsperreq, w, rowsperreq)
+		p = 1
+
+		for row = 1,rowsperreq do
+			for col = 1,w do
+				color = pixels[p]
+				of:write(string.format("%d\t%d\t%d\t", color[1], color[2], color[3]))
+				io.write(string.format("%d\t%d\t%d\n", color[1], color[2], color[3]))
+				p = p + 1
+			end
+			of:write("\n")
+		end
+	end
+	of:close()
 end
